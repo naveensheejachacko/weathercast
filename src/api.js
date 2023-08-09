ad1f346a2c71e46ab4c2c9de87eafbc93cb13892
naveen <naveenchacko1314@gmail.com>
2023-08-09 14:08:47 +0530

    first

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9a41cbacdbmsh1741c15bc3f12b5p1b88fcjsn7cc3b486ec1e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3878160b5b20704906137efc69f37c94";
